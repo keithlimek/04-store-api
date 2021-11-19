119ccf617a7d58a3ad1de1b04e2c882915bc1c5f
keithlim <keithlimek@gmail.com>
2021-11-19 21:19:42 +0800

    fixed bug with app.js router

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', (req,res) => {
     res.send('<h1>Store API</h1><a href= "/api/v1/products">products route</a>')
 })
 
-app.use('./api/v1/products', productsRouter)
+app.use('/api/v1/products', productsRouter)
 
 
 // products route
